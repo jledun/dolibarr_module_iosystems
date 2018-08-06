61b8f4aed737a641cab669c3da38554c1bd3ce0a
Julien Ledun <j.ledun@iosystems.fr>
2018-08-06 16:48:58 +0200

    test actions
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- class/actions_iosystems.class.php
+++ class/actions_iosystems.class.php
@@ -63,6 +63,14 @@ class Actionsiosystems
 	    $this->db = $db;
 	}
 
+        public function formObjectOptions($parameters, &$object, &$action, $hookmanager) {
+          global $langs, $conf, $form;
+          $TContext = explode(':', $parameters['context']);
+          if (in_array('propalcard', TContext) || in_array('ordercard', $TContext)) {
+            print("<h1>Je suis gros</h1>");
+          }
+        }
+
 	/**
 	 * Overloading the doActions function : replacing the parent's function with the one below
 	 *
